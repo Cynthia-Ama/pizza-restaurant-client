872947cfef900162551da4c046e64e1b1e1e3e39
Cynthia-Ama <amaehulec@gmail.com>
2023-08-25 20:32:20 +0100

    commit

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import styled from "styled-components"
+
+
+const ErrorContainer = styled.div`
+    padding: 40px;
+    width: 100%;
+
+    @media only screen and (min-width:800px){
+      padding: 30px 0;
+    }
+
+    @media only screen and (min-width:1000px){
+      padding: 50px 0;
+      max-width: 1200px;
+      margin: 0 auto;
+    }
+`
 
 export default function Error() {
   return (
-    <div className='error'>
+    <ErrorContainer className='error'>
       <h1>Page doesn't exist.</h1>
-    </div>
+    </ErrorContainer>
   )
 }
